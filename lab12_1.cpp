2fb3973657bf2b13bfb849c7193ebe97cf834beb
TIM <86812783+Theejuth@users.noreply.github.com>
2024-12-24 14:42:23 +0700

    LAB12_1 COMPLETE!

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
-int fibonacci(int x = 0);
+long long int fibonacci(long long int x);
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+int main() {
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int x){
-    if(x == 1 || x == 0){
+long long int fibonacci(long long int x) {  // Change parameter type to match the declaration
+    if (x == 1 || x == 0) {
         return x;
-    }else{
-        return fibonacci(x-1) + fibonacci(x-2);
+    } else {
+        x = fibonacci(x - 1) + fibonacci(x - 2);
+        return x;
     }
 }
